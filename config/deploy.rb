9ed4ca61029a2d19b392c3224799884d72fb8f85
SugiKen <ynmk1995@gmail.com>
2017-08-08 17:42:30 +0900

    deploy.rbを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,40 +16,56 @@ set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    invoke 'unicorn:restart'
+  task :default do
+    update
   end
+
+  task :update do
+    transaction do
+      update_code
+      create_config_symlink
+      create_symlink
+    end
+  end
+
+  task :start do
+    run "cd #{current_path} && bundle exec unicorn_rails -c #{current_path}/config/unicorn.rb -E #{rails_env} -D"
+  end
+
+  task :restart do
+    stop
+    start
+  end
+
+  task :reload do
+    if File.exist? "#{current_path}/tmp/pids/unicorn.pid"
+      run "kill -s USR2 `cat #{current_path}/tmp/pids/unicorn.pid`"
+    else
+      start
+    end
+  end
+
+  task :stop do
+    run "kill -s QUIT `cat #{current_path}/tmp/pids/unicorn.pid`"
+  end
+
+  task :cache_clear do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake tmp:cache:clear"
+  end
+
+  task :precompile, :roles => :web do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
+  end
+
+  task :create_config_symlink, :roles => :web do
+    on_rollback { run "rm -rf #{release_path}; true" }
+    run "cp #{release_path}/config/database.yml.base #{shared_path}/database.yml && ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
+    run "cp #{release_path}/config/unicorn/#{rails_env}.rb #{shared_path}/unicorn.rb && ln -s #{shared_path}/unicorn.rb #{release_path}/config/unicorn.rb"
+  end
+  # desc 'Restart application'
+  # task :restart do
+  #   invoke 'unicorn:restart'
+  # end
 end
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+after "deploy", "deploy:cleanup"
