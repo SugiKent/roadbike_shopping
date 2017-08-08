3ab1606f90dbab4a15c3707424497db741dd9ebe
SugiKen <ynmk1995@gmail.com>
2017-08-08 14:04:20 +0900

    価格変化のviewを変更

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+  def price_deference(prices)
+    return if prices.count <= 1
+    latest_price = prices.last
+    if prices[-2].price > latest_price.price
+      '<span class="text-primary">↓</span>'
+    elsif prices[-2].price == latest_price.price
+      '<span class="text-default">→</span>'
+    else
+      '<span class="text-danger">↑</span>'
+    end
+  end
 end
