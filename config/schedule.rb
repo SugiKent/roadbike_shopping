4b310472903426cba0c9c8ffcb9480f7e6f153a7
SugiKen <ynmk1995@gmail.com>
2017-08-08 23:17:04 +0900

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,7 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 env :PATH, ENV['PATH']
+set :environment, :production
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :output, 'log/cron.log'
 set :bundle_command, "/root/.rbenv/shims/bundle"
