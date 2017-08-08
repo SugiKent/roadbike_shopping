3cdb4ba6438c3b25b33cc4a417d6dc4edbd9e341
SugiKen <ynmk1995@gmail.com>
2017-08-08 23:46:20 +0900

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,21 +2,20 @@
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
-# env :PATH, ENV['PATH']
 env :GEM_PATH, ENV['GEM_PATH']
 set :environment, :production
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :output, 'log/cron.log'
 set :bundle_command, "/root/.rbenv/shims/bundle"
 
+# Could not find command "script/runner"のエラー対処のため
 job_type :custom_runner,
   "cd :path && /root/.rbenv/shims/bundle exec rails runner -e :environment ':task' :output"
 # Example:
 #
 # set :output, "/path/to/my/cron_log.log"
 #
-# every 1.day, at: '10:55pm' do # 昼の12時に配信
-every 4.minute do
+every 1.day, at: '11:59am' do # 昼の12時に配信
   custom_runner "ProductPrice.crawling_price"
 end
 #
